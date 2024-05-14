fe852f1cc0ffcf116514d0dff0d92503775a3a75
Sebastian Medina <sebasmedina2003@gmail.com>
2024-05-14 00:15:29 -0400

    refactor: estructura basica

--- main.py
+++ main.py
@@ -1,66 +1,13 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from uuid import uuid4
+import uvicorn
+import dotenv
+import os
+from fastapi import FastAPI
+from src.api.producto import router_productos
 
-from src.lib.managedb import ManageDB
-
-class Product(BaseModel):
-    id : str = str(uuid4()) 
-    name: str 
-    amount: int
-    price: float
-   
+dotenv.load_dotenv(override=True)
 
 app:FastAPI = FastAPI()
+app.include_router(prefix='/api/v1', router=router_productos)
 
-@app.get("/")
-def root():
-    return {"Hello": "World"} 
-
-@app.get("/api/products")
-def get_products():
-    return ManageDB().read_products()
-
-@app.get("/api/products/{product_id}")
-def get_single_products(product_id: str):
-    products: list = ManageDB().read_products()
-    for product in products:
-        if str(product["id"]) == product_id:
-            return product
-    raise HTTPException(status_code=404, detail="Product not found")
-
-@app.post("/api/products")
-def create_product(product: Product):
-    products: list = ManageDB().read_products()
-    products.append(product.dict())
-    ManageDB().write_products(products)
-    return {"Success":True,
-            "Message":"Product added successfully",
-            "Value":product.dict()
-            }
-
-@app.put("/api/products/{product_id}")
-def update_product(product_id: str, product: Product):
-    products: list = ManageDB().read_products()
-    for index, prod in enumerate(products):
-        if prod["id"] == product_id:
-            products[index] = product.dict()
-            ManageDB().write_products(products)
-            return {"Success":True,
-                    "Message":"Product updated successfully",
-                    "Value":product.dict()
-                    }
-    raise HTTPException(status_code=404, detail="Product not found")
-
-@app.delete("/api/products/{product_id}")
-def delete_product(product_id: str):
-    products: list = ManageDB().read_products()
-    for index, prod in enumerate(products):
-        if prod["id"] == product_id:
-            products.pop(index)
-            ManageDB().write_products(products)
-            return {"Success":True,
-                    "Message":"Product deleted successfully",
-                    "Value":product_id
-                    }
-    raise HTTPException(status_code=404, detail="Product not found")
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv('HOST', '0.0.0.0'), port=int(os.getenv('PORT', 8000)), log_level="debug")
---
+++ src/api/producto.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter, HTTPException
+from src.lib.managedb import ManageDB
+from pydantic import BaseModel
+from uuid import uuid4
+
+router_productos = APIRouter()
+
+class Product(BaseModel):
+    id : str = str(uuid4()) 
+    name: str 
+    amount: int
+    price: float
+
+
+@router_productos.get("/")
+def root():
+    return {"Hello": "World"} 
+
+@router_productos.get("/products")
+def get_products():
+    return ManageDB().read_products()
+
+@router_productos.get("/products/{product_id}")
+def get_single_products(product_id: str):
+    products: list = ManageDB().read_products()
+    for product in products:
+        if str(product["id"]) == product_id:
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@router_productos.post("/products")
+def create_product(product: Product):
+    products: list = ManageDB().read_products()
+    products.router_productosend(product.dict())
+    ManageDB().write_products(products)
+    return {"Success":True,
+            "Message":"Product added successfully",
+            "Value":product.dict()
+            }
+
+@router_productos.put("/products/{product_id}")
+def update_product(product_id: str, product: Product):
+    products: list = ManageDB().read_products()
+    for index, prod in enumerate(products):
+        if prod["id"] == product_id:
+            products[index] = product.dict()
+            ManageDB().write_products(products)
+            return {"Success":True,
+                    "Message":"Product updated successfully",
+                    "Value":product.dict()
+                    }
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@router_productos.delete("/products/{product_id}")
+def delete_product(product_id: str):
+    products: list = ManageDB().read_products()
+    for index, prod in enumerate(products):
+        if prod["id"] == product_id:
+            products.pop(index)
+            ManageDB().write_products(products)
+            return {"Success":True,
+                    "Message":"Product deleted successfully",
+                    "Value":product_id
+                    }
+    raise HTTPException(status_code=404, detail="Product not found")
